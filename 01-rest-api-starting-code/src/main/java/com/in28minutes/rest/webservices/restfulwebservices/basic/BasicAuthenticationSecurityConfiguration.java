ce8f7d146bd047597191a5e6106d27964a7d16aa
cesarpb80 <cesarpb80@gmail.com>
2023-04-26 17:20:19 -0500

    Se agrega JWT Authentication

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -1,14 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//NOTA DESABILITAR CONFIGURACION AUTH BASIC
+//@Configuration
 public class BasicAuthenticationSecurityConfiguration {
 
 	//Filter Chain
